e04c5dcf4845a6fc63cf4432c65e93c81e712fc0
Muhammad Afnan Hassan <151924900+OtakuTotipotent@users.noreply.github.com>
2025-02-11 15:23:19 +0500

    page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
-const toggle_btn = document.getElementById('toggle-open');
-const sidebar = document.getElementById('side-nav');
 
-toggle_btn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-})
-sidebar.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-})
+//! NAV TOGGLE ACTION
+const menu_open_btn = document.getElementById('toggle-open');
+const sidebar = document.getElementById('sidebar');
+const menu_close_btn = document.getElementById("toggle-close");
+
+menu_open_btn.addEventListener('click', () => {
+    sidebar.classList.toggle('toggle-active');
+});
+
+menu_close_btn.addEventListener('click', () => {
+    sidebar.classList.remove('toggle-active');
+});
