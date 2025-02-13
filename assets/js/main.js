4bb8091474b454506833e96a5568eef564086ba9
Muhammad Afnan Hassan <151924900+OtakuTotipotent@users.noreply.github.com>
2025-02-13 11:40:40 +0500

    sidebar home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-//! NAV TOGGLE ACTION
+//! NAV TOGGLE ACTION IS WRITTEN HERE
 const menu_open_btn = document.getElementById('toggle-open');
 const sidebar = document.getElementById('sidebar');
 const menu_close_btn = document.getElementById("toggle-close");
@@ -10,4 +10,30 @@ menu_open_btn.addEventListener('click', () => {
 
 menu_close_btn.addEventListener('click', () => {
     sidebar.classList.remove('toggle-active');
+});
+
+//! ACTIVE NAVBAR INDICATION CODE GOES HERE
+
+const links = document.body.querySelectorAll("nav a");
+const current_url = window.location.href;
+links.forEach((link) => {
+    if (link.href === current_url) {
+        link.classList.add("active-link");
+    }
+});
+
+//! ACTIVE DROPDOWN MENU ACTION IS HANDLED HERE
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".dropdown-btn button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            let dropdown_list = button.parentElement.nextElementSibling;
+            dropdown_list.classList.toggle("dropdown-activated")
+            if (!dropdown_list) {
+                console.error("Dropdown list is not found for ,", this);
+                return;
+            }
+        });
+    });
 });
